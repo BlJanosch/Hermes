f676bb88f146f40c67e4364a2414b997aa82cecf
Jannik <jannik-eduard.laengle@student.htl-rankweil.at>
2025-05-07 12:28:46 +0200

    Added Flutter Project

---
+++ src/frontend/hermes/lib/user.dart
@@ -0,0 +1,12 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
+class User {
+  int ID;
+  String Benutzername;
+  String Passwort;
+  String Profilbild;
+  double Hoehenmeter;
+  double KMGelaufen;
+
+  User(this.ID, this.Benutzername, this.Passwort, this.Profilbild, {this.Hoehenmeter = 0, this.KMGelaufen = 0});
+}
