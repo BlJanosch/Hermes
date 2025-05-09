9da36c317d387a0100a41eea28e244519b39815a
Jannik <jannik-eduard.laengle@student.htl-rankweil.at>
2025-05-09 10:04:26 +0200

    Added Map to HomePage

--- src/frontend/hermes/lib/components/bottom_nav_bar.dart
+++ src/frontend/hermes/lib/components/bottom_nav_bar.dart
@@ -7,19 +7,29 @@ class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GNav(
-        tabs: [
-          GButton(
-            icon: Icons.home,
-            text: "Home",
-          ),
-          GButton(
-            icon: Icons.card_membership,
-            text: "Sammelkarten",
-          ),
-          GButton(icon: Icons.leaderboard, text: "Leaderboard",),
-          GButton(icon: Icons.settings, text: "User",),
-        ],
+      height: 100,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: GNav(
+          backgroundColor: Colors.black.withOpacity(0.5),
+          color: Colors.grey[400],
+          activeColor: Colors.grey.shade700,
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabBackgroundColor: Colors.grey.shade100,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 10,
+          tabMargin: EdgeInsets.symmetric(horizontal: 10),
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+            ),
+            GButton(
+              icon: Icons.collections,
+            ),
+            GButton(icon: Icons.leaderboard),
+            GButton(icon: Icons.settings),
+          ],
+        ),
       ),
     );
   }
