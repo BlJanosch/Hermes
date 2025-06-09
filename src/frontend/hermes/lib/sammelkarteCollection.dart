ad0409f7fc7eea15c0844d928096acd619f695c7
Noah Fedele <noah.fedele@student.htl-rankweil.at>
2025-06-09 19:47:07 +0200

    Filter funktion hinzugefügt (collection page)

--- src/frontend/hermes/lib/sammelkarteCollection.dart
+++ src/frontend/hermes/lib/sammelkarteCollection.dart
@@ -4,4 +4,22 @@ class SammelkarteCollection {
   List<Sammelkarte> sammelkarten = [];
 
   SammelkarteCollection();
+
+  void sortierenNach(String kriterium) {
+    switch (kriterium.toLowerCase()) {
+      case 'seltenheit':
+        sammelkarten.sort((a, b) => b.schwierigkeit.index.compareTo(a.schwierigkeit.index));
+        break;
+      case 'neueste':
+        sammelkarten.sort((a, b) => b.Datum.compareTo(a.Datum));
+        break;
+      case 'name':
+        sammelkarten.sort((a, b) => a.Name.compareTo(b.Name));
+        break;
+      case 'höhe':
+      case 'hoehe':
+        sammelkarten.sort((a, b) => a.Hoehe.compareTo(b.Hoehe));
+        break;
+    }
+  }
 }
