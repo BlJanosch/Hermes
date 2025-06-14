e540428a809ae15de31b4360d756583e0fa73c8b
BlJanosch <167287612+BlJanosch@users.noreply.github.com>
2025-06-14 14:04:01 +0200

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hermes
+
+## Login/Register
+![](doc/BedienungsanleitungBilder/Screenshot_20250614_135337.png)
--- src/frontend/hermes/lib/components/globals.dart
+++ src/frontend/hermes/lib/components/globals.dart
@@ -2,5 +2,5 @@ library my_globals;
 
 import 'package:logger/logger.dart';
 
-String serverIP = "100.76.123.83";
+String serverIP = "10.0.0.27";
 var logger = Logger();
